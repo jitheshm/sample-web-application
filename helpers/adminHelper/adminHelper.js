80c5d64e56ee02f352dbcb8594f6e874daa80e28
Jithesh M <jitheshmjithooz@gmail.com>
2023-12-07 21:39:13 +0530

    admin authentication added

---
+++ helpers/adminHelper/adminHelper.js
@@ -0,0 +1,22 @@
+const db=require('../database/config')
+const bcrypt = require('bcrypt');
+module.exports={
+    login:(data)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection('admin').findOne({userId:data.userId}).then((result)=>{
+                if(result!=null){
+                    bcrypt.compare(data.password, result.password).then((result)=>{
+                        if(result){
+                            resolve({success:true})
+                        }else{
+                            resolve({success:false})
+                        }
+                    });
+                }else{
+                    resolve({success:false})
+                }
+
+            })
+        })
+    }
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,27 @@
 var express = require('express');
+const { login } = require('../helpers/adminHelper/adminHelper');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get('/login', function (req, res, next) {
+  if(req.session.adminError){
+    var error="user name or password is incorrect"
+    req.session.adminError=false
+  }
+  res.render('admin/login',{error})
 });
+
+router.post('/login', (req, res) => {
+  login(req.body).then((result) => {
+    if (result.success) {
+      req.session.adminStatus=true
+      res.redirect("/admin")
+    }else{
+      req.session.adminError=true
+      res.redirect("/admin/login")
+    }
+  })
+
+})
 
 module.exports = router;
