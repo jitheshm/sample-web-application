e1b98694d95ec4d52be29a3b12fc3f3ffbc0741d
jitheshm <jitheshmjithooz@gmail.com>
2023-12-26 11:12:03 +0530

    check email is already exist or not before signup and bugs in database connection is fixed

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,10 +2,21 @@ const { MongoClient } = require("mongodb");
 var database;
 module.exports = {
     connect: () => {
-        const uri = "mongodb://localhost:27017";
-        const client = new MongoClient(uri);
-        database=client.db('sample-webApplication')
-        console.log("connected to database");
+        return new Promise((resolve, reject) => {
+            const uri = "mongodb://localhost:27017";
+            const client = new MongoClient(uri);
+            client.connect().then(() => {
+                
+                database = client.db('sample-webApplication')
+                console.log("connected to database");
+                resolve()
+            }).catch((err) => {
+                console.log("error:"+err);
+                reject()
+            })
+
+        })
+
 
     },
     get: () => {
--- helpers/userHelper/userHelper.js
+++ helpers/userHelper/userHelper.js
@@ -4,20 +4,20 @@ const saltRounds = 10;
 module.exports = {
     signup: (data) => {
         return new Promise((resolve, reject) => {
+            db.get().collection('user').findOne({ email: data.email }).then((result) => {
+                if (!result) {
+                    bcrypt.hash(data.password, saltRounds).then(function (hash) {
+                        // Store hash in your password DB.
+                        data.password = hash;
+                        db.get().collection('user').insertOne(data).then((result) => {
+                            resolve("success")
+                        })
+                    });
+                } else {
+                    reject("email is already registered")
+                }
+            })
 
-            bcrypt.hash(data.password, saltRounds).then(function (hash) {
-                // Store hash in your password DB.
-                data.password = hash;
-                db.get().collection('user').insertOne(data).then((result) => {
-                    resolve("success")
-                }).catch((err)=>{
-                    if(err.code===11000){
-                        reject("email is already registered")
-                    }else{
-                        reject("error")
-                    }
-                })
-            });
         })
     },
     login: (data) => {
@@ -26,10 +26,12 @@ module.exports = {
                 if (result != null) {
                     bcrypt.compare(data.password, result.password).then((res) => {
                         if (res) {
-                            resolve({ success: true,data:{
-                                name:result.name
+                            resolve({
+                                success: true, data: {
+                                    name: result.name
 
-                            }})
+                                }
+                            })
                         } else {
                             resolve({ success: false })
                         }
